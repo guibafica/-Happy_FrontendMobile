10403cba2a21cede742ccdb73ba1b203dd007886
guibafica <gb.engcomp@hotmail.com>
2020-11-10 20:45:42 -0300

    ADD: Header da aplicacao

--- src/routes.tsx
+++ src/routes.tsx
@@ -10,10 +10,15 @@ import OrphanegeDetails from './pages/OrphanegeDetails';
 import OrphanageData from './pages/CreateOrphanage/OrphanageData';
 import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
 
+import Header from './components/Header';
+
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Navigator screenOptions={{ headerShown: false }} >
+      <Navigator screenOptions={{ 
+        headerShown: false, 
+        cardStyle: { backgroundColor: '#f2f3f5' } }} 
+      >
         <Screen 
           name="OrphanagesMap" 
           component={OrphanagesMap} 
@@ -22,11 +27,32 @@ export default function Routes() {
           // }}
         />
         
-        <Screen name="OrphanegeDetails" component={OrphanegeDetails} />
+        <Screen 
+          name="OrphanegeDetails" 
+          component={OrphanegeDetails}
+          options={{
+            headerShown: true,
+            header: () => <Header showCancel={false} title="Orfanato" />
+          }} 
+        />
 
-        <Screen name="SelectMapPosition" component={SelectMapPosition} />
+        <Screen 
+          name="SelectMapPosition" 
+          component={SelectMapPosition}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Selecione no mapa" />
+          }}  
+        />
 
-        <Screen name="OrphanageData" component={OrphanageData} />
+        <Screen 
+          name="OrphanageData" 
+          component={OrphanageData} 
+          options={{
+            headerShown: true,
+            header: () => <Header title="Informe os dados" />
+          }} 
+        />
       </Navigator>
     </NavigationContainer>
   );
